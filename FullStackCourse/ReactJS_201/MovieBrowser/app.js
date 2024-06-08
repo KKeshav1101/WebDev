e0fe2f63ead9faca5d2f7964d6b92506e9be61fa
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-06-08 19:32:14 +0530

    Initial Commit- Movie Browser Project

---
+++ FullStackCourse/ReactJS_201/MovieBrowser/app.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import { useState, useEffect } from 'react';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AboutView from "./components/AboutView";
+import SearchView from './components/SearchView';
+import MovieView from './components/MovieView';
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    if(searchText) {
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=57e1d61d512d515dcd91d6b3d5d1546f&language=en-US&query=${searchText}&page=1&include_adult=false`)
+        .then(response => response.json())
+        .then(data => {
+          setSearchResults(data.results)
+        })
+    }
+  }, [searchText])
+
+
+  return (
+    <div>
+      <Navbar searchText={searchText} setSearchText={setSearchText} />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" component={AboutView} />
+        <Route path="/search">
+          <SearchView keyword={searchText} searchResults={searchResults} />
+        </Route>
+        <Route path="/movies/:id" component={MovieView} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
